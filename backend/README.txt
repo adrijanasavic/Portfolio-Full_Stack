93a56ece8e5fa98602bc98963581d3ba7d646727
adrijanasavic <adrijana.work@gmail.com>
2023-02-27 12:47:59 +0100

    database connected

--- backend/README.txt
+++ backend/README.txt
@@ -1,2 +1,3 @@
 npm i express nodemon mongoose --save 
 npm i dotenv
+npm i cors // CORS is a node. js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 
 const PORT = process.env.PORT;
+const MONGO_URL = process.env.MONGO_URL;
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(MONGO_URL, (err) => {
+    if (!err) {
+        console.log("Database connected...");
+    } else {
+        console.log(err);
+    }
+})
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
     console.log(`http://localhost${PORT}`);
